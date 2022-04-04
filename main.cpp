ae9ee9910837aaf393fe45788ff6a1bda1f6b540
Mar-Q_global <goodMorning_global@Mar-Q.com>
2022-04-05 05:35:48 +0800

    实现画线

--- main.cpp
+++ main.cpp
@@ -1,9 +1,75 @@
 #include "tgaimage.h"
+#include "model.h"
+#include "geometry.h"
+#include<vector>
 const TGAColor red = TGAColor(255, 0, 0, 255);
+const TGAColor white = TGAColor(255, 255, 255, 255);
+const TGAColor green = TGAColor(0, 255, 0, 255);
+const int width = 1000;
+const int height = 1000;
+Model* model = nullptr;
+void line(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color) {
+	//Bresenham’s line algorithm(推导过程基于0<k<1)
+	//key:(x,y)后下一个点只有可能是(x,y)或(x,y±1)
+	bool steep = false;
+	if (std::abs(x1 - x0) < std::abs(y1 - y0)) {
+		steep = true;
+		std::swap(x0, y0);
+		std::swap(y1, x1);
+	}
+	if (x0 > x1) {//下面for循环是从x0到x1的，确保x0<x1
+		std::swap(x0, x1);
+		std::swap(y0, y1);
+	}
+	int dx = x1 - x0;
+	int dy = y1 - y0;
+	int delta_y = 1;
+	if (dy < 0) {//斜率为负的情况也统一视作为正斜率来处理，只要让y的变化量为-1
+		dy = -dy;
+		delta_y = -1;
+	}
+	int error = 0;
+	int y = y0;
+	for (int x = x0; x <= x1; x++) {
+		if (steep) {
+			image.set(y, x, color);
+		}
+		else {
+			image.set(x, y, color);
+		}
+		error += dy;
+		if (error * 2 >= dx) {
+			y += delta_y;
+			error -= dx;
+		}
+	}
+}
+void test(TGAImage& image) {
+	line(13, 20, 80, 40, image, white);
+	line(20, 13, 40, 80, image, red);
+	line(80, 40, 13, 20, image, red);
+	line(0, 0, 100, 100, image, green);
+}
 int main(int argc, char** argv) {
-	TGAImage image(100, 100, TGAImage::RGB);
-	image.set(10, 41, red);
-	image.flip_vertically(); // i want to have the origin at the left bottom corner of the image
-	image.write_tga_file("output.tga");
+	//TGAImage image(100, 100, TGAImage::RGB);
+	//test(image);
+	model = new Model("obj/african_head/african_head.obj");
+
+	TGAImage image(width, height, TGAImage::RGB);
+	for (int i = 0; i < model->nfaces(); i++) {
+		std::vector<int> face = model->face(i);
+		for (int j = 0; j < 3; j++) {
+			Vec3f v0 = model->vert(face[j]);
+			Vec3f v1 = model->vert(face[(j + 1) % 3]);
+			//以下相当于乘以视口变换矩阵
+			int x0 = (v0.x + 1.) * width / 2.;
+			int y0 = (v0.y + 1.) * height / 2.;
+			int x1 = (v1.x + 1.) * width / 2.;
+			int y1 = (v1.y + 1.) * height / 2.;
+			line(x0, y0, x1, y1, image, white);
+		}
+	}
+	image.flip_vertically();
+	image.write_tga_file("face.tga");
 	return 0;
 }
