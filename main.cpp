6ce3fcd1438cee87bb6efd9de75b3b4794596d42
Mar-Q_global <goodMorning_global@Mar-Q.com>
2022-04-10 23:09:39 +0800

    lesson3:使用重心坐标对纹理坐标，深度进行插值。zbuffer

--- main.cpp
+++ main.cpp
@@ -8,8 +8,8 @@
 const TGAColor red = TGAColor(255, 0, 0, 255);
 const TGAColor white = TGAColor(255, 255, 255, 255);
 const TGAColor green = TGAColor(0, 255, 0, 255);
-const int width = 1000;
-const int height = 1000;
+const int width = 1024;
+const int height = 1024;
 Model* model = nullptr;
 void line(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color) {
 
@@ -53,17 +53,22 @@ inline void line(Vec2i t0, Vec2i t1, TGAImage &image, TGAColor color) {
 	return line(t0.x, t0.y, t1.x, t1.y, image, color);
 }
 
-Vec3f barycentric(Vec2i* tri,Vec2i point) {//返回该点的重心坐标（x，y，z） 几何意义：（Sa/（Sa+Sb+Sc），...，...） 可用于插值
+Vec3f barycentric(Vec3f* tri,Vec2i point) {//返回该点的重心坐标（x，y，z） 几何意义：（Sa/（Sa+Sb+Sc），...，...） 可用于插值
 	Vec3f res = Vec3f(tri[1].x - tri[0].x, tri[2].x - tri[0].x, tri[0].x - point.x) ^ Vec3f(tri[1].y - tri[0].y, tri[2].y - tri[0].y, tri[0].y - point.y);
-	if (std::abs(res.z) < 1) {//防止res.z为0作为除数
+	if (std::abs(res.z) < 1e-2) {//防止res.z为0作为除数
 		return Vec3f(-1, -1, -1);//返回一个非法的（在三角形外的点的）重心坐标
 	}
-	return Vec3f(1.f - (res.x + res.y) / res.z, res.x / res.z, res.y/ res.z);//重心坐标(1-u-v,u,v) res是和(u,v,1)平行的向量
+	return Vec3f(1.f - (res.x + res.y) / res.z, res.x / res.z, res.y/ res.z);//重心坐标为(1-u-v,u,v)。res是和(u,v,1)平行的向量
 }
-void triangle(Vec2i* tri, TGAImage &image, TGAColor color) {
-	line(tri[0] ,tri[1], image, color);
-	line(tri[1], tri[2], image, color);
-	line(tri[2], tri[0], image, color);
+//Vec3f barycentric(Vec3f* v, Vec2i point) { 验证了一下games101中 公式法求重心坐标 也正确
+//	float x = point.x;
+//	float y = point.y;
+//	float c1 = (x*(v[1].y - v[2].y) + (v[2].x - v[1].x)*y + v[1].x*v[2].y - v[2].x*v[1].y) / (v[0].x*(v[1].y - v[2].y) + (v[2].x - v[1].x)*v[0].y + v[1].x*v[2].y - v[2].x*v[1].y);
+//	float c2 = (x*(v[2].y - v[0].y) + (v[0].x - v[2].x)*y + v[2].x*v[0].y - v[0].x*v[2].y) / (v[1].x*(v[2].y - v[0].y) + (v[0].x - v[2].x)*v[1].y + v[2].x*v[0].y - v[0].x*v[2].y);
+//	float c3 = (x*(v[0].y - v[1].y) + (v[1].x - v[0].x)*y + v[0].x*v[1].y - v[1].x*v[0].y) / (v[2].x*(v[0].y - v[1].y) + (v[1].x - v[0].x)*v[2].y + v[0].x*v[1].y - v[1].x*v[0].y);
+//	return Vec3f(c1,c2,c3);
+//}
+void triangle(Vec3f* tri,Vec2i* txe,TGAImage &image, float light_k,float *z_buffer) {//这里把参数Vec2i改成Vec3i，把深度这一属性也带上，才能得到每个点插值后的深度
 	//计算三角形的矩形边界
 	int x_min = std::min(tri[0].x, std::min(tri[1].x, tri[2].x));
 	int y_min = std::min(tri[0].y, std::min(tri[1].y, tri[2].y));
@@ -72,49 +77,56 @@ void triangle(Vec2i* tri, TGAImage &image, TGAColor color) {
 	for (int x = x_min; x <= x_max; x++) {
 		for (int y = y_min; y <= y_max; y++) {
 			Vec3f bct = barycentric(tri, Vec2i (x, y));
+			float z = 0.0;
 			if (bct.x < 0 || bct.y < 0 || bct.z < 0) continue;
-			image.set(x, y, color);
+			z += bct.x*tri[0].z;
+			z += bct.y*tri[1].z;
+			z += bct.z*tri[2].z;
+			float u = 0.0;
+			float v = 0.0;
+			for (int i = 0; i < 3; i++) {
+				u += bct[i] * txe[i].x;
+				v += bct[i] * txe[i].y;
+			}
+			TGAColor c = model->diffuse(Vec2i(static_cast<int>(u), static_cast<int>(v)));
+			if (z > z_buffer[x + y * width]) {
+				z_buffer[x + y * width] = z;
+				image.set(x, y,TGAColor(c.r*light_k, c.g*light_k, c.b*light_k,255));
+			}
 		}
 	}
 }
 
 int main(int argc, char** argv) {
-	TGAImage image1(200,200, TGAImage::RGB);
-	Vec2i t0[3] = { Vec2i(10, 70),   Vec2i(50, 160),  Vec2i(70, 80) };
-	Vec2i t1[3] = { Vec2i(180, 50),  Vec2i(150, 1),   Vec2i(70, 180) };
-	Vec2i t2[3] = { Vec2i(180, 150), Vec2i(120, 160), Vec2i(130, 180) };
-	triangle(t0, image1, red);
-	triangle(t1, image1, white);
-	triangle(t2, image1, green);
-	image1.flip_vertically();
-	image1.write_tga_file("test.tga");
-
 	model = new Model("obj/african_head/african_head.obj");
 	Vec3f light(0, 0, -1);
 	TGAImage image(width, height, TGAImage::RGB);
+	float* z_buffer = new float[width*height];
+	for (int i = 0; i < width*height; i++) {
+		z_buffer[i] = -std::numeric_limits<float>::max();
+	}
 	for (int i = 0; i < model->nfaces(); i++) {
 		std::vector<int> face = model->face(i);
-		Vec2i tri[3];
-		Vec3f tri_withZ[3];
+		Vec3f screen_tri[3];//把深度属性z带上
+		Vec2i tex_arr[3];
+		Vec3f world_tri[3];
 		for (int j = 0; j < 3; j++) {
 			Vec3f v0 = model->vert(face[j]);
-			tri_withZ[j] = v0;
-			//Vec3f v1 = model->vert(face[(j + 1) % 3]);
+			world_tri[j] = v0;
+			tex_arr[j] = model->uv(i, j);
 			//model里的顶点都在[-1,1]立方体内了，相当于已经经过透视变换
 			//之后再乘以视口变换矩阵即可
 			int x0 = (v0.x + 1.) * width / 2.;
 			int y0 = (v0.y + 1.) * height / 2.;
-			//int x1 = (v1.x + 1.) * width / 2.;
-			//int y1 = (v1.y + 1.) * height / 2.;
-			//line(x0, y0, x1, y1, image, white); 在线上的点重心坐标也非负，在triangle里也会画，就不必重复画了
-			tri[j].x = x0;
-			tri[j].y = y0;
+			screen_tri[j].x = x0;
+			screen_tri[j].y = y0;
+			screen_tri[j].z = v0.z;
 		}
-		Vec3f normal_vector =(tri_withZ[2]-tri_withZ[0])^ (tri_withZ[1] - tri_withZ[0]);
+		Vec3f normal_vector =(world_tri[2]- world_tri[0])^ (world_tri[1] - world_tri[0]);
 		normal_vector.normalize();
 		float k = light * normal_vector.normalize();
 		if (k > 0) {
-			triangle(tri, image, TGAColor(k * 255, k * 255, k * 255, 255));
+			triangle(screen_tri, tex_arr, image,k,z_buffer);
 		}
 	}
 	image.flip_vertically();
--- model.h
+++ model.h
@@ -1,26 +1,26 @@
-//
-//  model.hpp
-//  tinyrenderer
-//
-//  Created by skychx on 2020/11/15.
-//
-
 #ifndef __MODEL_H__
 #define __MODEL_H__
 
 #include <vector>
 #include "geometry.h"
+#include "tgaimage.h"
 
 class Model {
 private:
-	std::vector<Vec3f> verts_;
-	std::vector<std::vector<int> > faces_;
+	std::vector<Vec3f> verts_;//顶点
+	std::vector<std::vector<Vec3i> > faces_; // attention, this Vec3i means vertex/uv/normal
+	std::vector<Vec3f> norms_;//对应的法向量
+	std::vector<Vec2f> uv_;//对应的纹理顶点
+	TGAImage diffusemap_;
+	void load_texture(std::string filename, const char *suffix, TGAImage &img);
 public:
 	Model(const char *filename);
 	~Model();
 	int nverts();
 	int nfaces();
 	Vec3f vert(int i);
+	Vec2i uv(int iface, int nvert);
+	TGAColor diffuse(Vec2i uv);
 	std::vector<int> face(int idx);
 };
 
